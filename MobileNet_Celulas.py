043ca5252208d16fe3d01c3c5d954e9a22bbc3a0
rodrigoangulo-tech <rodrigo.angulo@ucb.edu.bo>
2026-09-23 17:00:55 -0400

    Add files via upload
    Ahora se valida después de cada época de entrenamiento. Se muestra en un gráfico la evolución del costo.

--- MobileNet_Celulas.py
+++ MobileNet_Celulas.py
@@ -2,10 +2,14 @@ import torch
 import torchvision.models as modelos
 import torch.nn as nn
 from pathlib import Path
+from torchmetrics import MetricCollection
 from torchvision import datasets, transforms
 from torchsummary import summary
 from torch.utils.data import DataLoader
 import torch.optim as optim
+import torchmetrics as tm
+import matplotlib.pyplot as plt
+import ModeloBMN
 
 dir = Path.cwd()
 dir1 = dir / 'Celulas' / 'DataSet' / 'Entrenamiento'
@@ -16,34 +20,29 @@ print(dir2)
 #print("Funciona la  GPU? ", torch.cuda.is_available())
 dispositivo = "cuda" if torch.cuda.is_available() else "cpu"
 
+mobilenet3 = modelos.mobilenet_v3_large(weights = True) #Se requiere preentrenamiento
 
-mobilenet3 = modelos.mobilenet_v3_large(weights = True)
-#print("Dispositivo del modelo", next(modelos.parameters().device))
-#mobilenet3sc = mobilenet3d
-#mobilenet3sc = mobilenet3sc.to(dispositivo) #CNN para prueba
 modelo = mobilenet3
-#modelo = mobilenet3.features
-modelo.classifier[3] = nn.Linear(in_features=1280, out_features=5)#Numero de salidas 5: NILM, LSIL, HSIL, ASC y SCC
+modelo = mobilenet3.features
 
-'''
-Cabeza de  clasificador basado en trabajo de Ocampo et al.
+#modelo.classifier[3] = nn.Linear(in_features=1280, out_features=5)#Numero de salidas 5: NILM, LSIL, HSIL, ASC y SCC
+
 clasf = nn.Sequential(
     nn.AdaptiveAvgPool2d((1,1)),
     nn.Flatten(),
     nn.Linear(960,512),nn.LeakyReLU(),nn.Linear(512,128),nn.LeakyReLU(),nn.Linear(128,5), nn.LeakyReLU()
 )
-'''
-#modelo.classifier = clasf
+
+modelo.classifier = clasf
 modelo = modelo.to(dispositivo)
 
-#Un tensor de prueba para verifciar correcta estructura del modelo
+#Un tensor de prueba para verificar correcta estructura del modelo
 p = torch.rand((1,3,224,224))
 p = p.to(dispositivo)
 y = modelo(p)
 print(y)
 
 summary(modelo, input_size=(3,224,224))
-
 
 transformacion1 = transforms.Compose([
     transforms.Resize(224),
@@ -76,10 +75,18 @@ summary(mobilenet3d, input_size=(3,224,224))
 #summary(mobilenet3d, input_size=(3,224,224))
 
 '''
+metricas = tm.MetricCollection([
+    tm.Accuracy(task = "multiclass", num_classes=5),tm.Precision(task="multiclass",num_classes=5),
+    tm.Recall(task="multiclass",num_classes=5),
+    tm.Specificity(task="multiclass",num_classes=5)])
+metricas = metricas.to(dispositivo)
+costo_entr = []
+costo_val = []
 
-modelo.train()
- #entrenamiento
+
 for epoca in range(num_epocas):
+    # entrenamiento
+    modelo.train()
     costo_acumulado = 0.0
     for entradas, etiquetas in cargar_entrm:
         entradas = entradas.to(dispositivo)
@@ -94,26 +101,39 @@ for epoca in range(num_epocas):
         optimizador.step()
 
         costo_acumulado += costo.item()
+
+    costo_entr.append(costo.item())
     print(f"Epoca [{epoca}],Costo: {costo_acumulado/len(cargar_entrm):.2f}")
 
-dirA = dir / 'Modelo_Celulas.pth'
-torch.save(modelo,dirA)
+    #Evaluacion
+    modelo.eval()
+    correcto=0.0
+    total= 0.0
 
-#Evaluacion
-modelo.eval()
-correcto=0.0
-total= 0.0
+    with torch.no_grad():
+        for entradas, etiquetas in cargar_val:
+            entradas = entradas.to(dispositivo)
+            etiquetas = etiquetas.to(dispositivo)
+            salidas = modelo(entradas)
+            costov = criterio(salidas, etiquetas)
+            az,prediccion = torch.max(salidas.data,1)
 
-with torch.no_grad():
-    for entradas, etiquetas in cargar_val:
-        entradas = entradas.to(dispositivo)
-        etiquetas = etiquetas.to(dispositivo)
-        salidas = modelo(entradas)
-        az,prediccion = torch.max(salidas.data,1)
-        total += etiquetas.size(0)
-        correcto += (prediccion==etiquetas).sum().item()
+            total += etiquetas.size(0)
+            correcto += (prediccion==etiquetas).sum().item()
+
+        costo_val.append(costov.item())
 
     precision = 100*correcto/total
-    print(f' Precision de Validacion: {precision:.2f}%')
 
+print(f' Precision de Validacion: {precision:.2f}%') #precision simple
+dirA = dir / 'Modelo_Celulas.pth' #Guarda el modelo
+torch.save(modelo.state_dict(),dirA)
+
+print(costo_entr)
+print(costo_val)
+plt.plot(costo_entr,label = "Costo entrenamiento")
+plt.plot(costo_val,label="Costo validacion")
+plt.legend()
+plt.title("Costo por epoca")
+plt.show()
 
